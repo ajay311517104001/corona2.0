b566fabdb28bb773dfa5afab90ae919fb5ef7fd1
ajay311517104001 <ajaysaimageshwaran1999@gmail.com>
2020-05-07 11:43:13 +0530

    1commit

---
+++ client/build/precache-manifest.ca2402250156f469631245448b93b512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337baafabf5739aa3239ab6ef26575bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ec40891f84b8cde96e8",
+    "url": "/static/css/main.34ad10a2.chunk.css"
+  },
+  {
+    "revision": "f658c35e743141633ab5",
+    "url": "/static/js/2.61ae8ce1.chunk.js"
+  },
+  {
+    "revision": "8bb757413c38ce564bf5d6ba97eb4669",
+    "url": "/static/js/2.61ae8ce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec40891f84b8cde96e8",
+    "url": "/static/js/main.4130cd50.chunk.js"
+  },
+  {
+    "revision": "99a1df5724a42cc4fef3",
+    "url": "/static/js/runtime-main.4a3acf61.js"
+  }
+]);
--- server/app.js
+++ server/app.js
@@ -10,18 +10,21 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname,'client/build')))
 
-app.use('/',cors(),async function(req,res){
+
+app.use('/india',cors(),async function(req,res){
     await axios.get('https://api.covid19india.org/data.json')
     .then((response)=> { res.send(response.data) 
         }).catch((err)=>console.log(err))
     
  })
+if(process.env.NODE_ENV=='production'){
+    app.use(express.static(path.join(__dirname+'../client/build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname+'../client/build/index.html'))
+    })
+}
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'client/build/index.html'))
-})
 const PORT=process.env.PORT || 5000;
 app.listen( PORT,(err)=>{
     if(err){
